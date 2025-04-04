81618d09faf6316dc3cbf119c98034e03e2922f4
tobitchz <tojoaca10@gmail.com>
2025-04-04 18:12:05 -0300

    Ejercicio 1

---
+++ ejerciciosUML/ejerciciosUML/Ejercicio1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program 
+{ 
+    public static void Main(String[] args)
+    {
+
+        Autobus autobus = new Autobus();
+
+        autobus.capacidadPasajeros = 23;
+        autobus.cantPuertas = 3;
+        autobus.cantAsientos = 120;
+        autobus.numeroColectivo = "114";
+
+
+
+    }
+}
+
+public class Vehiculo
+{
+    public int capacidadPasajeros;
+    public int cantPuertas;
+    public int cantAsientos;
+
+}
+public class Autobus : Vehiculo 
+{
+    public string numeroColectivo;
+
+
+    public void SubidaDePasajeros()
+    {
+        Console.WriteLine("*El vehiculo se detiene y los pasajeros suben*");
+    }
+    public void BajadaDePasajeros()
+    {
+        Console.WriteLine("*El vehiculo se detiene y los pasajeros bajan*");
+    }
+
+}
--- ejerciciosUML/ejerciciosUML/obj/Release/net8.0/ejerciciosUML.AssemblyInfo.cs
+++ ejerciciosUML/ejerciciosUML/obj/Release/net8.0/ejerciciosUML.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejerciciosUML")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0e9df089b83de0cc402298d5269b6fafe78f16")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejerciciosUML")]
